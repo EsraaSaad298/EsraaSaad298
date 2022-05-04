61908105cff5f1cb7613ee7dd0ae568d73da0466
EsraaSaad298 <esraa_mohyeldin@hotmail.com>
2022-05-04 21:47:50 +0400

    Updates

---
+++ precache-manifest.e5fda21b62e345c7ef62788a9355c363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bf7b9f8a475a49b7a69cb3e174fe5f",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "43134755f15f98b600fe",
+    "url": "/dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "753189e92277d3926b4f",
+    "url": "/dev-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "43134755f15f98b600fe",
+    "url": "/dev-portfolio/static/js/2.34ab75af.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/dev-portfolio/static/js/2.34ab75af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753189e92277d3926b4f",
+    "url": "/dev-portfolio/static/js/main.cff97110.chunk.js"
+  },
+  {
+    "revision": "1e10e024f0899dac1144",
+    "url": "/dev-portfolio/static/js/runtime-main.ce1c534e.js"
+  }
+]);
